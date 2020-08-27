8beef7d79110a2e5cf49b483ec3222ffff84dd2a
ＴＲ <public@technotter.com>
2020-08-27 19:38:11 +0900

    Selectorプロジェクトに実装を追加

---
+++ caMon.ISelector/ISelector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace caMon
+{
+	public interface ISelector : IPages
+	{
+		/// <summary>別ページへの推移を要求するイベント</summary>
+		event EventHandler<PageChangeEventArgs> PageChangeRequest;
+	}
+
+	/// <summary>選択画面から実画面を表示するためのイベントで使用する引数のクラス</summary>
+	public class PageChangeEventArgs : EventArgs
+	{
+		/// <summary>次に表示するページ</summary>
+		public IPages NewPage;
+
+		/// <summary>次に表示するページが実装されたmodファイルへのパス</summary>
+		public string ModPath;
+	}
+}
