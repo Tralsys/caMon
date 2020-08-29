4facf1e1c1c19fbfcee158d1fc283c3488723f5f
ＴＲ <public@technotter.com>
2020-08-30 00:27:05 +0900

    OBVEデータ表示ページを実装

--- caMon.pages.sample/Pages/Page_OBVE.xaml.cs
+++ caMon.pages.sample/Pages/Page_OBVE.xaml.cs
@@ -7,9 +7,13 @@ namespace caMon.pages.sample
 	/// </summary>
 	public partial class Page_OBVE : Page
 	{
+		BIDSData_toBind.OBVED_toBind OtB = new BIDSData_toBind.OBVED_toBind();
+
 		public Page_OBVE()
 		{
 			InitializeComponent();
+
+			DataContext = OtB;//OpenDをBindingできるように
 
 			SharedFuncs.SML.SMC_OpenDChanged += SML_SMC_OpenDChanged;//openBVE Dataが更新された際に実行される処理を登録する
 		}
@@ -17,9 +21,6 @@ namespace caMon.pages.sample
 		/// <summary>openBVE Dataが更新された際に実行されるように登録するメソッド</summary>
 		/// <param name="sender">この関数の呼び出し元(通知するかは呼び出し元の実装次第)</param>
 		/// <param name="e">実行に関連する情報が格納された引数</param>
-		private void SML_SMC_OpenDChanged(object sender, TR.ValueChangedEventArgs<OpenD> e)
-		{
-			
-		}
+		private void SML_SMC_OpenDChanged(object sender, TR.ValueChangedEventArgs<OpenD> e) => OtB.OD = e.NewValue;
 	}
 }
