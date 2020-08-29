36dba015f0cf2f6b8746765131c5772f56239538
ＴＲ <public@technotter.com>
2020-08-29 20:39:29 +0900

    Binding使用のためのプロジェクト追加

---
+++ BIDSData_toBind/OnPropChangedIncluded.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace BIDSData_toBind
+{
+	public class OnPropChangedIncluded : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged(string s) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(s));
+	}
+}
