26c486378bd56f9d4ef8ff809fe3e09e0b66b455
ＴＲ <public@technotter.com>
2021-03-28 13:35:43 +0900

    Add Index-Setting Load/Save Function

--- caMon.pages.BIDSDataUpdateNotifierUsecase/Lamps/ATSP.xaml.cs
+++ caMon.pages.BIDSDataUpdateNotifierUsecase/Lamps/ATSP.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 using BIDSDataUpdateNotifier.LampStateProvider;
 
@@ -14,5 +16,30 @@ namespace caMon.pages.BIDSDataUpdateNotifierUsecase.Lamps
 			DataContext = new ATSPLamps();
 			InitializeComponent();
 		}
+
+		private void SaveSetting(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				(DataContext as ATSPLamps)?.SaveIndexesToXML();
+			}catch(Exception ex)
+			{
+				MessageBox.Show(ex.ToString(), typeof(ATSP).FullName);
+			}
+			ActionCompletedLabel.Visibility = Visibility.Visible;
+		}
+		private void LoadSetting(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				(DataContext as ATSPLamps)?.LoadIndexesFromXML();
+			}catch(Exception ex)
+			{
+				MessageBox.Show(ex.ToString(), typeof(ATSP).FullName);
+			}
+			ActionCompletedLabel.Visibility = Visibility.Visible;
+		}
+
+		private void ClickToHideDo(object sender, System.Windows.Input.MouseButtonEventArgs e) => (sender as UIElement).Visibility = Visibility.Collapsed;
 	}
 }
